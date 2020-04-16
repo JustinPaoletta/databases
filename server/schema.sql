4b37cb9ff5dc171f741322c5423c982684afdf47
Justin Paoletta <justinpaoletta@gmail.com>
2020-04-15 23:47:30 -0400

    working on Persistent Storage for Chatterbox App

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,28 @@
+-- DROP TABLE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  MessageID INT NOT NULL AUTO_INCREMENT,
+  Message_text VARCHAR(255),
+  Created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UserID INT,
+  Rooms VARCHAR(255),
+  PRIMARY KEY (MessageID)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  ID INT NOT NULL AUTO_INCREMENT,
+  Name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(ID)
+);
 
+ALTER TABLE messages ADD FOREIGN KEY(UserID) REFERENCES users(ID);
+
+/* Create other tables and define schemas for them here! */
 
 
 
